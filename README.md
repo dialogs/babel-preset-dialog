b72f79bcff8ca061b1d8471fb5bf549f1c7a8cca
Nikita Gusakov <gusnkt@gmail.com>
2016-09-05 00:42:59 +0300

    feat: add options and some new plugins

--- README.md
+++ README.md
@@ -16,7 +16,26 @@ Add `@dlghq/babel-preset-dialog` to your `.babelrc`.
 ```json
 {
   "presets": [
-    "@dlghq/babel-preset-dialog"
+    ["@dlghq/babel-preset-dialog", {
+      "strict": false,
+      "optimize": true,
+      "typecheck": true
+    }]
   ]
 }
 ```
+
+Options
+-------
+
+Name | Default | Description
+---- | ------- | -----------
+`spec` | `false` | Enable spec compliance
+`loose` | `true` | Enable loose mode
+`react` | `true` | Enable react transforms
+`es2015` | `true` | Enable ES2015 syntax
+`esnext` | `true` | Enable ESNext syntax
+`strict` | `true` | Add `"use strict";` to each file
+`minify` | `false` | Enable minification plugins
+`optimize` | `false` | Enable optimization plugins
+`typecheck` | `false` | Enable typecheck plugin
--- index.js
+++ index.js
@@ -1,56 +1,108 @@
-function loose(plugin) {
-  return [plugin, { loose: true }];
+const defaultOptions = {
+  spec: false,
+  loose: true,
+  react: true,
+  es2015: true,
+  esnext: true,
+  strict: true,
+  minify: false,
+  optimize: false,
+  typecheck: false
+};
+
+function preset(context, options) {
+  const {
+    spec,
+    loose,
+    react,
+    es2015,
+    esnext,
+    strict,
+    minify,
+    optimize,
+    typecheck
+  } = Object.assign({}, defaultOptions, options);
+
+  const plugins = [];
+  const presets = [];
+
+  if (strict) {
+    plugins.push(
+      require('babel-plugin-transform-strict-mode')
+    );
+  }
+
+  if (esnext) {
+    plugins.push(
+      require('babel-plugin-transform-class-properties'),
+      require('babel-plugin-transform-object-rest-spread'),
+      require('babel-plugin-transform-export-extensions')
+    );
+  }
+
+  if (react) {
+    plugins.push(
+      require('babel-plugin-syntax-jsx'),
+      require('babel-plugin-syntax-flow'),
+      require('babel-plugin-transform-react-jsx'),
+      require('babel-plugin-transform-flow-strip-types'),
+      require('babel-plugin-transform-react-display-name')
+    );
+  }
+
+  if (es2015) {
+    plugins.push(
+      [require('babel-plugin-transform-es2015-template-literals'), { loose, spec }],
+      require('babel-plugin-transform-es2015-literals'),
+      require('babel-plugin-transform-es2015-function-name'),
+      [require('babel-plugin-transform-es2015-arrow-functions'), { spec }],
+      require('babel-plugin-transform-es2015-block-scoped-functions'),
+      [require('babel-plugin-transform-es2015-classes'), { loose }],
+      require('babel-plugin-transform-es2015-object-super'),
+      require('babel-plugin-transform-es2015-shorthand-properties'),
+      require('babel-plugin-transform-es2015-duplicate-keys'),
+      [require('babel-plugin-transform-es2015-computed-properties'), { loose }],
+      [require('babel-plugin-transform-es2015-for-of'), { loose }],
+      require('babel-plugin-transform-es2015-sticky-regex'),
+      require('babel-plugin-transform-es2015-unicode-regex'),
+      require('babel-plugin-check-es2015-constants'),
+      [require('babel-plugin-transform-es2015-spread'), { loose }],
+      require('babel-plugin-transform-es2015-parameters'),
+      [require('babel-plugin-transform-es2015-destructuring'), { loose }],
+      require('babel-plugin-transform-es2015-block-scoping'),
+      require('babel-plugin-transform-es2015-typeof-symbol'),
+      [require('babel-plugin-transform-es2015-modules-commonjs'), { loose }]
+    );
+  }
+
+  if (typecheck) {
+    plugins.push(
+      require('babel-plugin-typecheck')
+    );
+  }
+
+  if (optimize) {
+    plugins.push(
+      require('babel-plugin-lodash')
+    );
+  }
+
+  if (optimize && react) {
+    plugins.push(
+      require('babel-plugin-transform-react-constant-elements'),
+      require('babel-plugin-transform-react-inline-elements')
+    );
+  }
+
+  if (minify) {
+    presets.push(
+      require('babel-preset-babili')
+    )
+  }
+
+  return {
+    plugins
+  };
 }
 
-module.exports = {
-  plugins: [
-    // Helpers
-    require('babel-plugin-transform-strict-mode'),
-
-    // ES Next
-    require('babel-plugin-transform-class-properties'),
-    require('babel-plugin-transform-object-rest-spread'),
-    require('babel-plugin-transform-export-extensions'),
-
-    // ES 2015
-    loose(require('babel-plugin-transform-es2015-template-literals')),
-    require('babel-plugin-transform-es2015-literals'),
-    require('babel-plugin-transform-es2015-function-name'),
-    require('babel-plugin-transform-es2015-arrow-functions'),
-    require('babel-plugin-transform-es2015-block-scoped-functions'),
-    loose(require('babel-plugin-transform-es2015-classes')),
-    require('babel-plugin-transform-es2015-object-super'),
-    require('babel-plugin-transform-es2015-shorthand-properties'),
-    require('babel-plugin-transform-es2015-duplicate-keys'),
-    loose(require('babel-plugin-transform-es2015-computed-properties')),
-    loose(require('babel-plugin-transform-es2015-for-of')),
-    require('babel-plugin-transform-es2015-sticky-regex'),
-    require('babel-plugin-transform-es2015-unicode-regex'),
-    require('babel-plugin-check-es2015-constants'),
-    loose(require('babel-plugin-transform-es2015-spread')),
-    require('babel-plugin-transform-es2015-parameters'),
-    loose(require('babel-plugin-transform-es2015-destructuring')),
-    require('babel-plugin-transform-es2015-block-scoping'),
-    require('babel-plugin-transform-es2015-typeof-symbol'),
-    loose(require('babel-plugin-transform-es2015-modules-commonjs')),
-
-    // React
-    require('babel-plugin-syntax-jsx'),
-    require('babel-plugin-syntax-flow'),
-    require('babel-plugin-transform-react-jsx'),
-    require('babel-plugin-transform-flow-strip-types'),
-    require('babel-plugin-transform-react-display-name')
-  ],
-  env: {
-    production: {
-      plugins: [
-        // Code optimizers
-        require('babel-plugin-lodash'),
-
-        // React optimizers
-        require('babel-plugin-transform-react-constant-elements'),
-        require('babel-plugin-transform-react-inline-elements')
-      ]
-    }
-  }
-};
+module.exports = preset;
